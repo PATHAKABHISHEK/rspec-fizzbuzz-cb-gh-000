5d3b874234d5711b3c32fc51144f32a12e4e610b
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-06-24 08:41:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(value)
-  if (value % 3 ==0 && value % 5 ==0)
+  if (value % 3 == 0 && value % 5 == 0)
     puts "FizzBuzz"
-  elsif (value % 3 ==0)
+  elsif (value % 3 == 0)
     puts "Fizz"
   elsif (value % 5 == 0)
     puts "Buzz"
