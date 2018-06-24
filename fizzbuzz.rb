72af02e325d3a057f2dfacc353b85d9cc8d39364
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-06-24 08:36:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizbuzz(value)
+def fizzbuzz(value)
   if (value % 3 ==0 && value % 5 ==0)
     puts ""
   elsif (value % 3 ==0)
